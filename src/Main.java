0bd9b34eae3d7b1d296f34d46bf64f9acba9ee7a
syhan11 <sueyoung.6311@gmail.com>
2019-06-13 09:01:19 -0400

    2nd commit: write my name

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
-    public static void main(String []args){
-        System.out.println ("Hello World!");
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Today is July 19, 2015. I am alive!");
+        System.out.println("My name is Sue.");
     }
 }
